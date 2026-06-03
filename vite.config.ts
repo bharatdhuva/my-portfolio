69a26c576f51500885e50287d3be863f2e186ba5
Your Name <you@example.com>
2026-06-03 19:08:14 +0530

    migration to vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: { entry: "src/server.ts" },
-    }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
