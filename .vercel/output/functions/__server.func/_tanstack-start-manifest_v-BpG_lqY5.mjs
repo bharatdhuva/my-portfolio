0403baaab91cc9d2d08f92ba75281086c22332de
Bharat Dhuva <bharatdhuva27@gmail.com>
2026-06-03 19:49:28 +0530

    solved render errors

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-BpG_lqY5.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/dhuva/Desktop/Bharat_Portfolio/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BvM8KnC0.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BvM8KnC0.js" } }] }, "/": { filePath: "C:/Users/dhuva/Desktop/Bharat_Portfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-D08TpVCD.js"] } } });
+export {
+  tsrStartManifest
+};
