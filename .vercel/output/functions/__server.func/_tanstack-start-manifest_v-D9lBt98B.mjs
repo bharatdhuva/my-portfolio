0ab4b85264c2489fc1b764834709b2e142ea80d2
Your Name <you@example.com>
2026-06-03 18:41:39 +0530

    chore: generate Vercel serverless function and Nitro configuration

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-D9lBt98B.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/dhuva/Desktop/Bharat_Portfolio/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index--GIf61Vi.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index--GIf61Vi.js" } }] }, "/": { filePath: "C:/Users/dhuva/Desktop/Bharat_Portfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BsAh3DiL.js"] } } });
-export {
-  tsrStartManifest
-};
